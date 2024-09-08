6999e3fd5eae5815c1f96294c246e2b2e442140b
Yusuf AKIN <akinyusuf17@gmail.com>
2024-09-08 22:43:44 +0300

    Add manifests

---
+++ tasks/week-2/server.py
@@ -0,0 +1,38 @@
+
+"""
+This is a simple HTTP server that serves the current time in seconds since the epoch
+Server:
+    Host: localhost
+    Port: 8080
+Endpoint:
+    GET /time
+"""
+
+import http
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+from time import time
+import json
+
+
+def get_current_time():
+    return {"time": time()}
+
+class Handler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        print(self.path)
+        # if self.path == '/time':
+        self.send_response(http.HTTPStatus.OK)
+        self.send_header('Content-type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')  # Allow requests from any origin
+        self.end_headers()
+        self.wfile.write(json.dumps(get_current_time()).encode())
+        # else:
+        #     super().do_GET()
+
+
+if __name__ == '__main__':
+    server = HTTPServer(('0.0.0.0', 8080), Handler)
+    print("serving at 0.0.0.0:8080")
+    server.serve_forever()
+
